4a94faebf933df18b6ae51400f1eab46fb98651c
Marieth <mperezzevallos@gmail.com>
2019-04-08 21:43:35 -0500

    Add methods for List series

--- app/controllers/api/series_controller.rb
+++ app/controllers/api/series_controller.rb
@@ -1,4 +1,10 @@
 class Api::SeriesController < ApplicationController
-
+    def index
+        if  params[:filter]
+            render json: Serie.where(status: params[:filter])
+        else       
+            render json: Serie.all
+        end
+    end
 
 end
--- spec/controllers/api/series_controller_spec.rb
+++ spec/controllers/api/series_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Api::SeriesController, type: :controller do
-
+    before do
+        @serie1 = Serie.create(title: "Serie1", status: "preorder")
+        @serie2 = Serie.create(title: "Serie2", status: "coming-soon")
+    end
+    
+    describe 'GET index' do
+        it 'returns http status ok' do
+          get :index
+          expect(response).to have_http_status(:ok)
+        end
+        it 'render json with all series' do
+          get :index
+          series = JSON.parse(response.body)
+          expect(series.size).to eq 2
+        end
+        it 'render json with all movies with filter status: "preorder" ' do
+            get :index, params: { filter: "preorder" }
+            series = JSON.parse(response.body)
+            expect(series.size).to eq 1
+        end
+        it 'render json with all movies with filter status: "coming-soon" ' do
+            get :index, params: { filter: "coming-soon" }
+            movies = JSON.parse(response.body)
+            expect(movies.size).to eq 1
+        end
+        it 'render json with all movies with filter status: "billboard" ' do
+            get :index, params: { filter: "billboard" }
+            movies = JSON.parse(response.body)
+            expect(movies.size).to eq 0
+        end
+      end
 end
